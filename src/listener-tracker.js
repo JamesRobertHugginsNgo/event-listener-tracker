728cd3bd1722d06c7a375b0d83304c7cfb423a37
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2026-08-28 18:57:29 -0400

    Add code

---
+++ src/listener-tracker.js
@@ -0,0 +1,101 @@
+function normalizeOptions(options) {
+	return options !== null && typeof options === 'object'
+		? options
+		: { capture: options };
+}
+
+export default class ListenerTracker {
+	_map = new Map(); // hint: [type][listener][capture] = { listener, signal, signalListener }
+
+	addListener(type, listener, options) {
+		const {
+			capture = false,
+			once = false,
+			signal
+		} = normalizeOptions(options);
+
+		const isAbortSignal = signal instanceof AbortSignal;
+		if (isAbortSignal && signal.aborted) {
+			return; // already aborted, don't even add
+		}
+
+		if (!this._map.has(type)) {
+			this._map.set(type, new Map());
+		}
+
+		const typeMap = this._map.get(type);
+		if (!typeMap.has(listener)) {
+			typeMap.set(listener, new Map());
+		}
+
+		const listenerMap = typeMap.get(listener);
+		if (!listenerMap.has(capture)) {
+			let signalListener;
+			if (isAbortSignal) {
+				signalListener = () => {
+					this.removeListener(type, listener, options);
+				};
+				signal.addEventListener('abort', signalListener, { once: true });
+			}
+
+			if (once) {
+				const removeListener = () => {
+					this.removeListener(type, listener, options);
+				};
+				listenerMap.set(capture, {
+					listener: function (...args) {
+						removeListener();
+						return typeof listener === 'function'
+							? listener.call(this, ...args)
+							: listener.handleEvent(...args);
+					},
+					signal,
+					signalListener
+				});
+			} else {
+				listenerMap.set(capture, { listener, signal, signalListener });
+			}
+		}
+
+		return listenerMap.get(capture).listener;
+	}
+
+	removeListener(type, listener, options) {
+		const { capture = false } = normalizeOptions(options);
+
+		let mappedListener;
+		if (this._map.has(type)) {
+			const typeMap = this._map.get(type);
+			if (typeMap.has(listener)) {
+				const listenerMap = typeMap.get(listener);
+				if (listenerMap.has(capture)) {
+					const {
+						listener: capturedListener,
+						signal,
+						signalListener
+					} = listenerMap.get(capture);
+
+					mappedListener = capturedListener;
+
+					if (signal instanceof AbortSignal) {
+						signal.removeEventListener('abort', signalListener, { once: true });
+					}
+
+					listenerMap.delete(capture);
+					if (listenerMap.size === 0) {
+						typeMap.delete(listener);
+						if (typeMap.size === 0) {
+							this._map.delete(type);
+						}
+					}
+				}
+			}
+		}
+
+		return mappedListener;
+	}
+
+	hasType(type) {
+		return this._map.has(type);
+	}
+}
---
+++ src/listener-tracker.test.js
@@ -0,0 +1,143 @@
+import ListenerTracker from './listener-tracker.js';
+
+import assert from 'node:assert';
+import test, { describe } from 'node:test';
+
+describe('listener-tracker.test.js', () => {
+	test('add listener', () => {
+		const tracker = new ListenerTracker();
+		const listener = () => void 0;
+
+		const result = tracker.addListener('change', listener);
+
+		assert.strictEqual(listener, result);
+		assert.strictEqual(tracker._map.get('change').size, 1);
+		assert.ok(tracker.hasType('change'));
+	});
+
+	test('add 2 listeners', () => {
+		const tracker = new ListenerTracker();
+		const listener1 = () => void 0;
+		const listener2 = () => void 0;
+
+		const result1 = tracker.addListener('change', listener1);
+		const result2 = tracker.addListener('change', listener2);
+
+		assert.strictEqual(listener1, result1);
+		assert.strictEqual(listener2, result2);
+		assert.strictEqual(tracker._map.get('change').size, 2);
+		assert.ok(tracker.hasType('change'));
+	});
+
+	test('add listener twice', () => {
+		const tracker = new ListenerTracker();
+		const listener = () => void 0;
+
+		tracker.addListener('change', listener);
+		tracker.addListener('change', listener);
+
+		assert.strictEqual(tracker._map.get('change').size, 1);
+		assert.ok(tracker.hasType('change'));
+	});
+
+	test('add listener as captured and non captured resulting in two listeners', () => {
+		const tracker = new ListenerTracker();
+		const listener = () => void 0;
+
+		tracker.addListener('change', listener);
+		tracker.addListener('change', listener, { capture: true });
+
+		const typeMap = tracker._map.get('change');
+		assert.strictEqual(typeMap.size, 1);
+
+		const listenerMap = typeMap.get(listener);
+		assert.ok(listenerMap.has(true));
+		assert.ok(listenerMap.has(false));
+
+		assert.ok(tracker.hasType('change'));
+	});
+
+	test('add "once" listener', () => {
+		const tracker = new ListenerTracker();
+		const listener = () => void 0;
+
+		const result = tracker.addListener('change', listener, { once: true });
+
+		assert.notStrictEqual(listener, result);
+		assert.strictEqual(tracker._map.get('change').size, 1);
+		assert.ok(tracker.hasType('change'));
+	});
+
+	test('add "once" listener then resolve by calling resulting listener ending with no listener', () => {
+		const tracker = new ListenerTracker();
+		const listener = () => void 0;
+
+		const result = tracker.addListener('change', listener, { once: true });
+		result();
+
+		assert.ok(!tracker._map.has('change'));
+		assert.ok(!tracker.hasType('change'));
+	});
+
+	test('add listener then abort ending with no listener', () => {
+		const tracker = new ListenerTracker();
+		const abortController = new AbortController();
+		const listener = () => void 0;
+
+		tracker.addListener('change', listener, { signal: abortController.signal });
+		abortController.abort();
+
+		assert.ok(!tracker.hasType('change'));
+	});
+
+	test('add listener after abort ending with no listener', () => {
+		const tracker = new ListenerTracker();
+		const abortController = new AbortController();
+		const listener = () => void 0;
+
+		abortController.abort();
+		const result = tracker.addListener('change', listener, { signal: abortController.signal });
+
+		assert.strictEqual(result, undefined);
+		assert.ok(!tracker.hasType('change'));
+	});
+
+	test('remove listener', () => {
+		const tracker = new ListenerTracker();
+		const listener = () => void 0;
+
+		tracker.addListener('change', listener);
+		tracker.removeListener('change', listener);
+
+		assert.ok(!tracker._map.has('change'));
+		assert.ok(!tracker.hasType('change'));
+	});
+
+	test('remove non existing listener ending with no error', () => {
+		const tracker = new ListenerTracker();
+		const listener = () => void 0;
+
+		const result = tracker.removeListener('change', listener);
+
+		assert.strictEqual(result, undefined);
+		assert.ok(!tracker.hasType('change'));
+	});
+
+	test('remove one of two added listeners (captured and non captured)', () => {
+		const tracker = new ListenerTracker();
+		const listener = () => void 0;
+
+		tracker.addListener('change', listener);
+		tracker.addListener('change', listener, { capture: true });
+		tracker.removeListener('change', listener, { capture: true });
+
+		const typeMap = tracker._map.get('change');
+		assert.strictEqual(typeMap.size, 1);
+
+		const listenerMap = typeMap.get(listener);
+		assert.ok(!listenerMap.has(true));
+		assert.ok(listenerMap.has(false));
+
+		assert.ok(tracker.hasType('change'));
+	});
+});
